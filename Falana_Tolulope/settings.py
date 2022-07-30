0ee3347a67a75f0895b21c22388209a3bebcf8e1
Tolu Falana <falanaft@gmail.com>
2022-07-30 20:33:37 +0100

    Edit settings.py

--- Falana_Tolulope/settings.py
+++ Falana_Tolulope/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*fh!zdd=q1ap6aep-*9+uign8qln*&t_a4)3$g_*7d)m1z#!db'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
